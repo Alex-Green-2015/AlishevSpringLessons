437be54fec0209fb5492e66d5e8d7d096223634b
Alex G <test@gmail.com>
2023-07-03 23:25:58 +0400

    Les 20

--- ru.green.avi.spring/src/main/ru/green/avi/spring/controllers/FirstController.java
+++ ru.green.avi.spring/src/main/ru/green/avi/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.green.avi.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,12 +12,53 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
 
+    @GetMapping("/calculator")
+    public String helloPage(HttpServletRequest request,
+                            Model model) {
+
+        String a = request.getParameter("a");
+        String b = request.getParameter("b");
+        String action = request.getParameter("action");
+
+        try {
+            double firstParam = Double.parseDouble(a);
+            double secondParam = Double.parseDouble(b);
+            double response;
+
+            switch (action) {
+                case "multiplication":
+                    response = firstParam * secondParam;
+                    model.addAttribute("response", Double.toString(response));
+                    break;
+                case "addition":
+                    response = firstParam + secondParam;
+                    model.addAttribute("response", Double.toString(response));
+                    break;
+                case "subtraction":
+                    response = firstParam - secondParam;
+                    model.addAttribute("response", Double.toString(response));
+                    break;
+                case "division":
+                    response = firstParam / secondParam;
+                    model.addAttribute("response", Double.toString(response));
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+        } catch (RuntimeException e) {
+            String response = "Error. Input data is incorrect";
+            model.addAttribute("response", response);
+        }
+
+        return "first/calculation";
+    }
+
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname",required = false) String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
-
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
