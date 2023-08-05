46eb9c0c3f69242e8f379c9b2b4c3cae3a39d6bf
Alex G <test@gmail.com>
2023-08-05 14:58:50 +0400

    Les 23 - logic

--- les_21/src/main/java/ru/green/avi/spring/controllers/PeopleController.java
+++ les_21/src/main/java/ru/green/avi/spring/controllers/PeopleController.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.green.avi.spring.dao.PersonDAO;
-
-import javax.servlet.http.HttpServletRequest;
+import ru.green.avi.spring.models.Person;
 
 
 @Controller
@@ -33,5 +33,21 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @ModelAttribute("welcome")
+    public String testMethod() {
+        return "Welcome to my application";
     }
 }
